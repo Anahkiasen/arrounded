1a656409c4c7e740754b4ea67a9c5474920610ed
Maxime Fabre <ehtnam6@gmail.com>
2013-11-02 02:33:15 +0100

    Add Crawler class

---
+++ src/Arrounded/Crawler.php
@@ -0,0 +1,124 @@
+<?php
+namespace Arrounded;
+
+use Arrounded\Traits\UsesContainer;
+use Illuminate\Support\Str;
+
+/**
+ * A basic class to extract routes
+ * from the application and crawler them
+ */
+class Crawler
+{
+	use UsesContainer;
+
+	/**
+	 * The routes to ignore
+	 *
+	 * @var array
+	 */
+	protected $ignored = array();
+
+	/**
+	 * Get the routes to test
+	 *
+	 * @return array
+	 */
+	public function getRoutes(array $additional = array())
+	{
+		$routes = $this->app['cache']->remember('crawler-routes', 60, function () {
+			$routes = array();
+
+			foreach ($this->app['router']->getRoutes() as $route) {
+				$method = $route->getMethods()[0];
+				$uri    = $route->getPath();
+
+				// Skip some routes
+				if ($method != 'GET' or Str::contains($uri, $this->ignored)) {
+					continue;
+				}
+
+				// Replace models with their IDs
+				if ($model = $this->extractModelFromUrl($uri)) {
+					foreach ($model::take(10)->get() as $model) {
+						$model    = $this->replacePatternByKey($uri, $model->id);
+						$routes[] = $this->app['url']->to($model);
+					}
+					continue;
+				}
+
+				$routes[] = $this->app['url']->to($uri);
+			}
+
+			return $routes;
+		});
+
+		return array_merge($routes, $additional);
+	}
+
+	/**
+	 * Get the routes in a PHPUnit-friendly forma
+	 *
+	 * @return array
+	 */
+	public function provideRoutes(array $additional = array())
+	{
+		$queue  = array();
+		$routes = $this->getRoutes($additional);
+
+		// Build provider
+		foreach ($routes as $key => $route) {
+			$queue[] = [$key, $route];
+		}
+
+		return $queue;
+	}
+
+	/**
+	 * Set the routes to ignore
+	 *
+	 * @param array $ignored
+	 */
+	public function setIgnored(array $ignored = array())
+	{
+		$this->ignored = $ignored;
+	}
+
+	////////////////////////////////////////////////////////////////////
+	/////////////////////////////// HELPERS ////////////////////////////
+	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Replace a model pattern by a key in an URL
+	 *
+	 * @param  string  $uri
+	 * @param  integer $key
+	 *
+	 * @return string
+	 */
+	protected function replacePatternByKey($uri, $key)
+	{
+		return preg_replace('/\{([^}]+)\}/', $uri, $key);
+	}
+
+	/**
+	 * Extract a model pattern in an URL
+	 *
+	 * @param  string $url
+	 *
+	 * @return string|false
+	 */
+	protected function extractModelFromUrl($url)
+	{
+		// Extract model
+		preg_match('/\{([^}]+)\}/', $uri, $pattern);
+		$model = Str::studly(array_get($pattern, 1));
+		$model = Str::singular($model);
+
+		if (class_exists($model) and is_subclass_of($model, 'Illuminate\Database\Eloquent\Model')) {
+			return $model;
+		}
+
+		return false;
+	}
+}
