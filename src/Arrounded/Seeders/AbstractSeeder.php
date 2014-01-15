5fb79f5c2305526032d6b73f27117c7b09572c44
Maxime Fabre <ehtnam6@gmail.com>
2014-01-15 15:10:53 +0100

    Add AbstractSeeder::forModels

--- src/Arrounded/Seeders/AbstractSeeder.php
+++ src/Arrounded/Seeders/AbstractSeeder.php
@@ -59,33 +59,54 @@ abstract class AbstractSeeder extends Seeder
 	}
 
 	////////////////////////////////////////////////////////////////////
-	/////////////////////////////// HELPERS ////////////////////////////
+	/////////////////////////////// SEEDERS ////////////////////////////
 	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Loop over the specified models
+	 *
+	 * @param string|array  $models
+	 * @param Closure       $closure
+	 * @param integer       $min
+	 * @param integer       $max
+	 *
+	 * @return void
+	 */
+	protected function forModels($models, Closure $closure, $min = 1, $max = null)
+	{
+		$max = $max ?: $min;
+
+		$models = (array) $models;
+		foreach ($models as $model) {
+			$model   = $this->models.$model;
+			$entries = $model::lists('id');
+			foreach ($entries as $entry) {
+				$this->times(function() use ($closure, $entry, $model) {
+					$closure($entry, $model);
+				}, $min, $max);
+			}
+		}
+	}
 
 	/**
 	 * Generate X entries
 	 *
 	 * @param  Closure $closure
-	 * @param integer  $min Minimum entries
-	 * @param integer  $max Maximum entries
+	 * @param integer  $min
+	 * @param integer  $max
 	 *
 	 * @return void
 	 */
 	protected function generateEntries(Closure $closure, $min = 5, $max = null)
 	{
-		if (!$max) {
-			$max = $min + 5;
-		}
-
 		// Execute the Closure n times
 		$entries = array();
-		$number  = $this->faker->randomNumber($min, $max);
-		for ($i = 0; $i <= $number; $i++) {
+		$this->times(function($i) use ($closure, &$entries) {
 			if ($entry = $closure($i)) {
 				$entry = $entry->getAttributes();
 				$entries[] = $entry;
 			}
-		}
+		}, $min, $max);
 
 		if (!empty($entries)) {
 			$model = get_called_class();
@@ -129,18 +150,14 @@ abstract class AbstractSeeder extends Seeder
 	 */
 	protected function randomModels($model, $min = 5, $max = null)
 	{
-		if (!$max) {
-			$max = $min + 5;
-		}
-
 		// Get a random number of elements
 		$model     = $this->models.$model;
 		$available = $model::lists('id');
 		$number    = $this->faker->randomNumber($min, $max);
 
-		for ($i = 0; $i <= $number; $i++) {
+		$this->times(function() use ($available, &$entries) {
 			$entries[] = $this->faker->randomElement($available);
-		}
+		}, $min, $max);
 
 		return $entries;
 	}
@@ -161,5 +178,29 @@ abstract class AbstractSeeder extends Seeder
 		}
 
 		return $this->faker->randomElement($models->lists('id'));
+	}
+
+	////////////////////////////////////////////////////////////////////
+	/////////////////////////////// HELPERS ////////////////////////////
+	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Execute an action from $min to $max times
+	 *
+	 * @param integer  $min
+	 * @param integer  $max
+	 * @param Closure  $closure
+	 *
+	 * @return void
+	 */
+	protected function times(Closure $closure, $min, $max = null)
+	{
+		// Define the number of times to loop over
+		$max   = $max ?: $min + 5;
+		$times = $this->faker->randomNumber($min, $max);
+
+		for ($i = 0; $i <= $times; $i++) {
+			$closure($i);
+		}
 	}
 }
