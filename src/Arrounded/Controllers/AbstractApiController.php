338928d674a02574a827432c712b848892c379a2
Maxime Fabre <ehtnam6@gmail.com>
2014-04-02 16:15:27 +0200

    Better wrap method

--- src/Arrounded/Controllers/AbstractApiController.php
+++ src/Arrounded/Controllers/AbstractApiController.php
@@ -60,15 +60,59 @@ abstract class AbstractApiController extends Controller
 	}
 
 	/**
-	 * Wrap one or more items in an API convention format
+	 * Wray items and format responses
 	 *
 	 * @param ArrayableInterface $items
 	 * @param integer            $statusCode
 	 *
 	 * @return Response
 	 */
-	protected function wrap(ArrayableInterface $items, $statusCode = 200)
+	public function wrap(ArrayableInterface $items, $statusCode = 200)
 	{
-		return Response::json($items, $statusCode);
+		$meta = array();
+
+		// If it's a single model, return it
+		if ($items instanceof Model) {
+			return Response::json($items, $statusCode);
+		}
+
+		// If we have a Paginator, split the data and metadata into two
+		if ($items instanceof Paginator) {
+			$meta  = $items->toArray();
+			$items = array_pull($meta, 'data');
+		}
+
+		// Build response
+		$response = $this->wrapWithResource($items);
+		if ($meta) {
+			$response['meta'] = $meta;
+		}
+
+		return Response::json($response, $statusCode);
+	}
+
+	/**
+	 * Wrap entries with a resource
+	 *
+	 * @param Collection $items
+	 * @param string     $resource
+	 *
+	 * @return array
+	 */
+	protected function wrapWithResource($items, $resource = null)
+	{
+		// Get and format resource
+		if (!$resource) {
+			$resource = $this->repository->getModel();
+			$resource = strtolower($resource);
+			$resource = Str::plural($resource);
+		}
+
+		// Unwrap any unwrapped object
+		if (method_exists($items, 'toArray')) {
+			$items = $items->toArray();
+		}
+
+		return array($resource => $items);
 	}
 }
