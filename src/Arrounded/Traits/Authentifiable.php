e46711377b8d94147aa3ed54fbbcbc74cd468efc
Maxime Fabre <ehtnam6@gmail.com>
2014-04-23 16:46:20 +0200

    Add Authentifiable

---
+++ src/Arrounded/Traits/Authentifiable.php
@@ -0,0 +1,89 @@
+<?php
+namespace Arrounded\Traits;
+
+use Hash;
+
+/**
+ * A model with Auth capabilities
+ */
+trait Authentifiable
+{
+	////////////////////////////////////////////////////////////////////
+	//////////////////////////// AUTHENTICATION ////////////////////////
+	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
+	{
+		return $this->getKey();
+	}
+
+	/**
+	 * Get the password for the user.
+	 *
+	 * @return string
+	 */
+	public function getAuthPassword()
+	{
+		return $this->password;
+	}
+
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
+	{
+		return $this->email;
+	}
+
+	/**
+	 * Get the token value for the "remember me" session.
+	 *
+	 * @return string
+	 */
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	/**
+	 * Set the token value for the "remember me" session.
+	 *
+	 * @param  string  $value
+	 * @return void
+	 */
+	public function setRememberToken($value)
+	{
+		$this->attributes['remember_token'] = $value;
+	}
+
+	/**
+	 * Get the column name for the "remember me" token.
+	 *
+	 * @return string
+	 */
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
+
+	////////////////////////////////////////////////////////////////////
+	///////////////////////////// ATTRIBUTES ///////////////////////////
+	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Hash password before save
+	 *
+	 * @param string $password
+	 */
+	public function setPasswordAttribute($password)
+	{
+		$this->attributes['password'] = Hash::make($password);
+	}
+}
