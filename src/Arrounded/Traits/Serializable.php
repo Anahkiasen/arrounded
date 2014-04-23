3b20c775aa0eb5268987ded90a4f2e753c0320c9
Maxime Fabre <ehtnam6@gmail.com>
2014-04-23 15:08:13 +0200

    Add Serializable

---
+++ src/Arrounded/Traits/Serializable.php
@@ -0,0 +1,54 @@
+<?php
+namespace Arrounded\Traits;
+
+use Illuminate\Support\Contracts\ArrayableInterface;
+
+/**
+ * A serializable element
+ */
+trait Serializable
+{
+	/**
+	 * Serialize an entity and cast some attributes
+	 *
+	 * @param ArrayableInterface|array $entity
+	 *
+	 * @return array
+	 */
+	public function serializeEntity($entity)
+	{
+		// Cast the entity itself
+		if ($entity instanceof ArrayableInterface) {
+			$entity = $entity->toArray();
+		}
+
+		// Cast attributes
+		if ($this->casts) {
+			foreach ($this->casts as $type => $attributes) {
+				$entity = $this->castAttributes($entity, $attributes, $type);
+			}
+		}
+
+		return $entity;
+	}
+
+	/**
+	 * Convert an array of properties to other types
+	 *
+	 * @param array  $attributes
+	 * @param array  $toConvert
+	 * @param string $type
+	 *
+	 * @return array
+	 */
+	public function castAttributes(array $attributes, array $toConvert, $type = 'boolean')
+	{
+		foreach ($toConvert as $converted) {
+			if (isset($attributes[$converted])) {
+				settype($attributes[$converted], $type);
+			}
+		}
+
+		return $attributes;
+	}
+}
