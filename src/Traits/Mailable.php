666fad227bc3e70c2e06f1411695a40cd76fa30f
Maxime Fabre <ehtnam6@gmail.com>
2014-06-02 11:32:00 +0200

    Add Mailable trait

---
+++ src/Traits/Mailable.php
@@ -0,0 +1,38 @@
+<?php
+namespace Arrounded\Traits;
+
+use Arrounded\Collection;
+use Mail;
+
+/**
+ * A trait for a mailable person
+ */
+trait Mailable
+{
+	/**
+	 * Send an email to a model
+	 *
+	 * @param string  $subject
+	 * @param string  $view
+	 * @param array   $data
+	 * @param boolean $condition
+	 *
+	 * @return void
+	 */
+	public function emailOn($subject, $view, $data = array(), $condition = true)
+	{
+		$recipient = $this->email;
+		if (!$condition or !$recipient) {
+			return;
+		}
+
+		// Serialize data
+		$data['user'] = $this;
+		$data = Collection::serialize($data);
+
+		// Send email
+		Mail::queue($view, $data, function ($message) use ($recipient, $subject) {
+			$message->suject($subject)->to($recipient);
+		});
+	}
+}
