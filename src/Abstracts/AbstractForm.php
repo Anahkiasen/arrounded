06200183d3ec046fad12aadc97bdc8959aefefaf
Maxime Fabre <ehtnam6@gmail.com>
2014-06-03 16:12:01 +0200

    Add Validation

---
+++ src/Abstracts/AbstractForm.php
@@ -0,0 +1,62 @@
+<?php
+namespace Arrounded\Abstracts;
+
+use Illuminate\Validation\Factory as Validator;
+
+/**
+ * A class representation of a form
+ */
+abstract class AbstractForm
+{
+	/**
+	 * The validator factory
+	 *
+	 * @var Validator
+	 */
+	protected $validator;
+
+	/**
+	 * The validation rules
+	 *
+	 * @var array
+	 */
+	protected $rules = [];
+
+	/**
+	 * Build a new form
+	 *
+	 * @param Validator $validator
+	 * @param Request   $request
+	 */
+	public function __construct(Validator $validator)
+	{
+		$this->validator = $validator;
+	}
+
+	/**
+	 * Validate an array of attributes
+	 *
+	 * @param array $attributes
+	 *
+	 * @return void
+	 */
+	public function validate(array $attributes = array())
+	{
+		// Get attributes and create Validator
+		$validation = $this->validator->make($attributes, $this->rules);
+
+		if ($validation->fails()) {
+			throw new ValidationException('Validation failed', $validation->getMessageBag());
+		}
+	}
+
+	/**
+	 * Get the rules in use
+	 *
+	 * @return array
+	 */
+	public function getRules()
+	{
+		return $this->rules;
+	}
+}
---
+++ src/Validation/ValidationException.php
@@ -0,0 +1,38 @@
+<?php
+namespace Arrounded\Validation;
+
+use Exception;
+use Illuminate\Support\MessageBag;
+
+class ValidationException extends Exception
+{
+	/**
+	 * The bound errors
+	 *
+	 * @var MessageBag
+	 */
+	protected $errors;
+
+	/**
+	 * Throw a new ValidationException
+	 *
+	 * @param string     $message
+	 * @param MessageBag $errors
+	 */
+	public function __construct($message, MessageBag $errors)
+	{
+		parent::__construct($message);
+
+		$this->errors = $errors;
+	}
+
+	/**
+	 * Get the bound errors
+	 *
+	 * @return MessageBag
+	 */
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+}
