a89a93a28158e81dec92e68f6c3c693bdfed586d
Maxime Fabre <ehtnam6@gmail.com>
2014-07-28 17:32:33 +0200

    Add Notifiable trait

---
+++ src/Traits/Notifiable.php
@@ -0,0 +1,37 @@
+<?php
+namespace Arrounded\Traits;
+
+trait Notifiable
+{
+	/**
+	 * Checks if the user wants to be notified of something
+	 *
+	 * @param string $notification
+	 *
+	 * @return bool
+	 */
+	public function notifiedOn($notification)
+	{
+		return (bool) array_get($this->notifications, $notification);
+	}
+
+	/**
+	 * Serialize and store notifications settings
+	 *
+	 * @param array $notifications
+	 */
+	public function setNotificationsAttribute($notifications)
+	{
+		$this->setJsonAttribute('notifications', $notifications);
+	}
+
+	/**
+	 * Unserialize and return notifications settings
+	 *
+	 * @return array
+	 */
+	public function getNotificationsAttribute()
+	{
+		return $this->getJsonAttribute('notifications');
+	}
+}
